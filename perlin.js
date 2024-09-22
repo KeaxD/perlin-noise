c2b8695d526e7f844346c3dd6a539ea2901f8305
KeaxD <keanuph@gmail.com>
2024-09-21 19:22:31 -0700

    add gradient colors depending on height

--- perlin.js
+++ perlin.js
@@ -1,3 +1,5 @@
+import { lerp, fade } from "./utils.js";
+
 export default class PerlinNoise {
   constructor() {
     this.permutation = this.generatePermutation();
@@ -26,10 +28,6 @@ export default class PerlinNoise {
     return gradients;
   }
 
-  fade(t) {
-    return t * t * t * (t * (t * 6 - 15) + 10);
-  }
-
   dotGridGradient(ix, iy, x, y) {
     const gradient =
       this.gradients[this.permutation[ix + this.permutation[iy % 256]] % 256];
@@ -44,33 +42,40 @@ export default class PerlinNoise {
     const y0 = Math.floor(y);
     const y1 = y0 + 1;
 
-    const sx = this.fade(x - x0);
-    const sy = this.fade(y - y0);
+    const sx = fade(x - x0);
+    const sy = fade(y - y0);
 
     const n0 = this.dotGridGradient(x0, y0, x, y);
     const n1 = this.dotGridGradient(x1, y0, x, y);
-    const ix0 = this.lerp(n0, n1, sx);
+    const ix0 = lerp(n0, n1, sx);
 
     const n0y = this.dotGridGradient(x0, y1, x, y);
     const n1y = this.dotGridGradient(x1, y1, x, y);
-    const ix1 = this.lerp(n0y, n1y, sx);
+    const ix1 = lerp(n0y, n1y, sx);
 
-    return this.lerp(ix0, ix1, sy);
-  }
-
-  lerp(a, b, t) {
-    return a + t * (b - a);
+    return lerp(ix0, ix1, sy);
   }
 
   colorMapping(value) {
-    if (value < 100) {
-      return { r: 0, g: 0, b: 104 };
-    }
+    const water = {
+      min: 0,
+      max: 255,
+    };
+
+    const land = {
+      min: 0,
+      max: 180,
+    };
+
+    let colorValue;
+
     if (value < 130) {
-      return { r: 0, g: 0, b: 205 };
+      colorValue = lerp(water.min, water.max, value / 128);
+      return { r: 0, g: 0, b: colorValue };
     }
     if (value < 145) {
-      return { r: 0, g: 158, b: 0 };
+      colorValue = lerp(land.min, land.max, value / 128);
+      return { r: 0, g: colorValue, b: 0 };
     }
     if (value < 160) {
       return { r: 0, g: 120, b: 0 };
